0425316df5cd85cdaba1844c72e07f96a3a1bae0
akikvadze <akikvadze@akikvadze2.geocell.local>
2018-06-11 19:37:49 +0400

    Finished first step

--- src/SOAP_Dilbert.java
+++ src/SOAP_Dilbert.java
@@ -1,6 +1,57 @@
+import java.net.ResponseCache;
+import java.rmi.RemoteException;
+
+import javax.xml.rpc.ServiceException;
+
+import org.apache.axis.AxisFault;
+
+import net.gcomputer.webservices.DilbertLocator;
+import net.gcomputer.webservices.DilbertSoap;
+import net.gcomputer.webservices.DilbertSoapStub;
+
+//import net.gcomputer.webservices.DilbertLocator;
 
 public class SOAP_Dilbert {
 	public static void main(String[] args) {
-		System.out.println("    SOAP Dilbert project ... ");	
+		
+		SOAP_Dilbert dilbert = new SOAP_Dilbert();
+		
+		dilbert.test();
+		
 	}
+
+	public void test() {
+		System.out.println("    SOAP Dilbert project ... ");
+		
+		DilbertLocator locator = new DilbertLocator();		
+		String soapAddress = locator.getDilbertSoapAddress();
+		
+		System.out.println(soapAddress);
+		
+		String serviceName = locator.getDilbertSoapWSDDServiceName();
+		System.out.println(serviceName);
+		
+		DilbertSoap soap1 = null;
+		try {
+			soap1 = new DilbertSoapStub();
+		} catch (AxisFault e1) {
+			e1.printStackTrace();
+		}		
+		try {
+			soap1 = locator.getDilbertSoap();
+		} catch (ServiceException e) {
+			e.printStackTrace();
+		}
+		
+		String responce = "";
+		
+		try {
+			responce = soap1.todaysDilbert();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(responce);
+	}
+	
 }
